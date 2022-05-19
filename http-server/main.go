f952b00337cc4286edfc556be8a3bb316a803cdc
Akirapearl <draculaspot@hotmail.com>
2022-05-19 12:18:31 +0200

    Added support for css files being readable

--- http-server/main.go
+++ http-server/main.go
@@ -1,3 +1,4 @@
+//https://gobyexample.com/http-servers
 package main
 
 import (
@@ -12,13 +13,14 @@ func hello(w http.ResponseWriter, req *http.Request) {
 
 func headers(w http.ResponseWriter, r *http.Request) {
 
-	http.ServeFile(w, r, "./files/index.html")
+    http.ServeFile(w, r, "./files/index.html")
 }
 
 func main() {
 
     http.HandleFunc("/hello", hello)
-    http.HandleFunc("/headers", headers)
+    //http.HandleFunc("/headers", headers) -- Calls
+    http.Handle("/", http.FileServer(http.Dir("./files")))
 
     http.ListenAndServe(":8090", nil)
 }
