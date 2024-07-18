ded6d009eb9128e8ea3f234c1193a0ce9c5f16e8
Akirapearl <draculaspot@hotmail.com>
2024-07-18 22:40:11 +0200

    Added syntax learning up to structs

--- 2024/exercises/codewars/main.go
+++ 2024/exercises/codewars/main.go
@@ -7,6 +7,14 @@ import (
 
 func HoopCount(n int) string {
 	// 8 kyu
+	/*
+			Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him.
+
+			Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message:
+
+		    If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+		    If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+	*/
 	if n >= 10 {
 		return "Great, now move on to tricks"
 	} else if n < 10 {
@@ -18,6 +26,17 @@ func HoopCount(n int) string {
 
 func Initials(name string) string {
 	// 8 kyu
+	/*
+		Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+		The output should be two capital letters with a dot separating them.
+
+		It should look like this:
+
+			Sam Harris => S.H
+
+			patrick feeney => P.F
+	*/
 	parts := strings.Split(name, " ")
 	if len(parts) != 2 {
 		return ""
--- 2024/exercises/other/main.go
+++ 2024/exercises/other/main.go
@@ -56,8 +56,93 @@ func main() {
 	fmt.Println(intSlice)
 
 	// Maps
+	// key:value pairs
+	var myMap map[string]uint8 = make(map[string]uint8)
+	fmt.Println(myMap)
+
+	var myMap2 = map[string]uint{"Adam": 23, "Ahsoka": 5}
+	fmt.Println(myMap2["Ahsoka"])
+	// if asking for a non existent value -- default is 0
+	var age, ok = myMap2["Jason"]
+	if ok {
+		fmt.Printf("The age is %v", age)
+	} else {
+		fmt.Println("Invalid name")
+	}
+	// remove a value from a map
+	delete(myMap2, "Adam")
+
+	//Loops
+	// Iterate over maps, arrays or slices - range
+	for name := range myMap2 {
+		age = myMap2[name]
+		fmt.Printf("Name: %v, Age: %v \n", name, age)
+	}
+
+	// "While loop"
+	i := 0
+	for i < 10 {
+		fmt.Println(i)
+		i++
+	}
+
+	for qa := 0; qa < 10; qa++ {
+		fmt.Println(qa)
+	}
+
+	// Strings, Runes, Bytes
+	var myString = "resume"
+	// rune --> int32 encoded alias
+	// runes are immutable
+	var indexed = myString[0] // Adds variable to an array
+	// it stores the value of the string
+	fmt.Println(myString)
+	fmt.Println(indexed)                     // returns 114
+	fmt.Printf("%v, %T\n", indexed, indexed) // value, type
+
+	// UTF-8
+	// strings - binary numbers (ascii)
+	// Own conversion pattern for non ascii characters
+
+	// Structs and interfaces
+	// Structs - Collection of fields
+	// SEE OUTSIDE OF MAIN FUNCTION
+	type myStruct0 struct {
+		X         int
+		Y         int
+		Z         int
+		int             // only defining a type by its variable type
+		ownerInfo owner //outside of main function
+	}
+
+	//fmt.Println(myStruct0{1, 2, 3, 4})
+	fmt.Println(owner{"Owner"})
+	/*
+		v := myStruct0{1, 2}
+		v.X = 4
+		fmt.Println(v.X)
+	*/
+	var mynum myStruct0 = myStruct0{X: 100, Y: 200, ownerInfo: owner{"Alex"}} //asigning values to multiple fields at a time
+	mynum.Z = 300
+	mynum.int = 2 //single field value assignment
+	fmt.Println(mynum.X, mynum.Y, mynum.Z, mynum.int, mynum.ownerInfo.name)
+
+	// Struct methods
+	// Functions directly related to a struct
+	var myEngine gasEngine = gasEngine{25, 15} // "Class" is created/instanceated
+	fmt.Println(myEngine.mpg, myEngine.litres)
+	fmt.Printf("Total miles: %v", myEngine.milesleft()) //Calling method/"class"
+
+	// interfaces
+	// pending to understand
+
 }
 
+//	================
+//
+// NON MAIN FUNCTIONS
+//
+//	================
 func printMe(printValue string) {
 	fmt.Println(printValue)
 }
@@ -72,4 +157,19 @@ func intDivision(numerator int, denominator int) (int, int) {
 	result := numerator / denominator
 	var remainder int = numerator % denominator
 	return result, remainder
+}
+
+// structs
+
+type owner struct {
+	name string
+}
+type gasEngine struct {
+	mpg    uint8
+	litres uint8
+}
+
+func (e gasEngine) milesleft() uint8 {
+	// (e gasEngine) assigns the function to the type
+	return e.litres * e.mpg
 }
