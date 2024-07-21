c21479d90225329ee78996c9b870e19bc12e3fb1
Akirapearl <draculaspot@hotmail.com>
2024-07-21 16:16:41 +0200

    New exercise - JSON parser

---
+++ 2024/exercises/JSON_parser/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("This is a JSON parser")
+	rand(100)
+
+	/*
+			Exercise: Parse JSON
+
+		    Task: Write a program that reads a JSON string representing a list
+		    of people (with fields for name and age) and parses it into a slice
+		    of structs. Then, print out the names and ages of all people in the list.
+	*/
+
+}
