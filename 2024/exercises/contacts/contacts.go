7aa66232f8a675af19396251b06226c3d61f9411
Akirapearl <draculaspot@hotmail.com>
2024-07-22 23:35:00 +0200

    Improved contacts exercise

--- 2024/exercises/contacts/contacts.go
+++ 2024/exercises/contacts/contacts.go
@@ -6,14 +6,18 @@ import (
 )
 
 /*
-   Exercise: Simple Contact List
+	   Exercise: Simple Contact List
 
-       Task: Create a program that allows the user to add and view contacts.
-	   Each contact should have a name and a phone number.
-       Use a map to store the contacts, where the key is the contact's name
-       and the value is the phone number.
-
+	       Task: Create a program that allows the user to add and view contacts.
+		   Each contact should have a name and a phone number.
+	       Use a map to store the contacts, where the key is the contact's name
+	       and the value is the phone number.
 */
+func randNumber() int {
+	// Create a "default" value -- Generate a random number between 0 and 1000
+	num := rand.Intn(1000)
+	return num
+}
 
 func main() {
 	fmt.Println("Contacts exercise")
@@ -21,20 +25,27 @@ func main() {
 	// initialize map
 	contacts := make(map[string]int)
 
-	// Create a "default" value
-	num := rand.Intn(1000) // Generate a random number between 0 and 99
+	contacts["Alex"] = randNumber()
 
-	contacts["Alex"] = num
-
-	fmt.Println(contacts["Alex"])
+	//fmt.Println(contacts["Alex"])
 
 	// Reads input & generates variable for the input requested
 	fmt.Println("Input name:")
+
 	var w1 string
 	fmt.Scanln(&w1)
+	// Assign number to new element of the map
+	// New call to the function - Generates different number
+	contacts[w1] = randNumber()
 
-	// Assigns a number to it && prints it
-	contacts[w1] = num
-	fmt.Println("read line: ", contacts[w1])
+	// Print entire map
+	fmt.Println("map:", contacts)
+
+	// Prints assigned number only
+	fmt.Println("New contact has number: ", contacts[w1])
+
+	// Remove element from the map, then list content to verify
+	delete(contacts, w1)
+	fmt.Println("map:", contacts)
 
 }
