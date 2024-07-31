adb57f80da7f352cb5ba95b0115f8dcd4ac33d80
Akirapearl <draculaspot@hotmail.com>
2024-07-31 11:29:53 +0200

    Finished JSON parser exercise

--- 2024/exercises/JSON_parser/main.go
+++ 2024/exercises/JSON_parser/main.go
@@ -12,56 +12,66 @@ import (
 Exercise: Parse JSON
 
 Task: Write a program that reads a JSON string representing a list
-of people (with fields for name and age) and parses it into a struct. --> before it was an slice of structs
+of people (with fields for name and age) and parses it into a slice of structs.
 Then, print out the names and ages of all people in the list.
 
-STATUS: PENDING
+STATUS: COMPLETED
 */
-func main() {
-	fmt.Println("This is a JSON parser")
-	// Read entire file at once
-	content, err := os.ReadFile("read.json")
 
+func check(err error) error {
 	if err != nil {
 		// Throw error if the file is NULL
 		log.Fatal(err)
 	}
+	return err
+}
+
+func main() {
+	fmt.Println("This is a JSON parser")
+	// Read entire file at once
+	content, err := os.ReadFile("read.json")
+	check(err)
+
 	// Print content
 	fmt.Println(string(content))
 
 	// Go over the document word by word
 	file, err2 := os.Open("read.json")
-
-	if err2 != nil {
-		log.Fatal(err2)
-	}
+	check(err2)
 
 	scan := bufio.NewScanner(file)
 	scan.Split(bufio.ScanWords)
 
 	// Parse JSON into a struct
-
 	// JSON decoder
 	parser := json.NewDecoder(file)
-	var person people
 
+	var person people = people{}
+	/*
+		p := people{
+			Name: "Kaneda",
+			Age:  30,
+			City: "Neo Tokyo",
+		}
+	*/
 	err = parser.Decode(&person)
 	if err != nil {
+
 		log.Fatalf("Failed to decode JSON: %s", err)
 	}
 
 	// reflect --> It enables you to examine the type and value of variables dynamically.
 
-	fmt.Printf("Name: %s\n", person.name)
-	fmt.Printf("Age: %d\n", person.age)
-	fmt.Printf("City: %s\n", person.city)
+	fmt.Printf("Name: %s\n", person.Name)
+	fmt.Printf("Age: %d\n", person.Age)
+	fmt.Printf("City: %s\n", person.City)
 
 	file.Close()
 }
 
 // Struct declaration
 type people struct {
-	name string
-	age  uint8
-	city string
+	Name string
+	Age  uint8
+	City string
 }
--- 2024/exercises/batch_2/exercise4/exercise4.go
+++ 2024/exercises/batch_2/exercise4/exercise4.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 /*
 Exercise 4: Sorting Algorithms
@@ -12,6 +15,15 @@ Task: Implement a simple sorting algorithm (e.g., Bubble Sort) to sort a list of
 	Call bubbleSort to sort the slice.
 	Print the sorted slice.
 */
+
+func bubbleSort(slice []int) {
+	sort.Ints(slice)
+
+}
 func main() {
 	fmt.Println("Batch 2 - Exercise 4")
+	myslice := []int{1, 6, 4, 3, 5, 2}
+	fmt.Println(len(myslice))
+	bubbleSort(myslice)
+	fmt.Println(myslice)
 }
