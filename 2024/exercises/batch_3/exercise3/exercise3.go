ff20abda695f443aaf34231223415dd450e3bb5f
Akirapearl <draculaspot@hotmail.com>
2024-08-05 16:45:25 +0200

    Added exercises 2 and 3 batch 3

---
+++ 2024/exercises/batch_3/exercise3/exercise3.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+/*
+Exercise 3: Environment Variables
+
+Task: Write a program that reads environment variables and prints them.
+
+    Use the os.Getenv function to read specific environment variables.
+    Define a list of common environment variables to read (e.g., HOME, PATH, USER).
+    Loop through the list and print each environment variable and its value.
+    Handle cases where the environment variable is not set.
+*/
+
+func main() {
+	fmt.Println("Batch 3 - Exercise 3")
+
+}
