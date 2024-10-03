ef095ac27481f57a8238d92300628294d19d1fac
Akirapearl <draculaspot@hotmail.com>
2024-10-03 18:26:45 +0200

    Continued with Lets Go book - Chapter 3

---
+++ 2024/web_learning/letsgo/README.md
@@ -0,0 +1,5 @@
+## IMPORTANT NOTE
+
+The main.go file in the "root" directory for the let's go folder is a unchanged file containing some comments and notes for learning purposes, the actual main.go which is expected to runfor this project is placed within the following directory:
+
+/letsgo/cmd/web/
--- 2024/web_learning/letsgo/cmd/web/main.go
+++ 2024/web_learning/letsgo/cmd/web/main.go
@@ -2,17 +2,44 @@ package main
 
 // requires to run as go run ./cmd/web (from root of the project)
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// Define a command-line flag with default value ":4000" w/some help text
+	// Value will be stored in the addr variable at runtime
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// Parse command-line flag
+	// Reads value and assings it to addr variable
+	// otherwise - always contain default value :4000
+	// if errror - application will be terminated
+	flag.Parse()
+
 	mux := http.NewServeMux()
+
+	// Create a file server - ./ui/static directory
+	// The path given to the http.Dir function is relative
+	// to the project directory root
+
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	// mux.Handle() - register the file server as handler for
+	// all URL paths that startart with /static/
+
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	// Register the other application routes as normal
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
-	log.Print("starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+
+	log.Printf("starting server on %s", *addr)
+
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
+
 }
